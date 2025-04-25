ffb67950ee623ec78768a14f54a7ae7d8323a4e8
Qianvenh <3294416164@qq.com>
2025-04-25 18:49:34 +0800

    Finishing Employee pageQuery

---
+++ drink-tea-common/src/main/java/com/drinktea/properties/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.drinktea.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("drinktea.jwt")
+public class JwtProperties {
+    private String adminSecretKey;
+    private long adminTtl;
+    private String adminTokenName;
+}
---
+++ drink-tea-server/src/main/java/com/drinktea/DrinkTeaApplication.java
@@ -0,0 +1,21 @@
+package com.drinktea;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@Slf4j
+//@EnableCaching
+//@EnableScheduling
+public class DrinkTeaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkTeaApplication.class, args);
+        log.info("Server started");
+    }
+}
