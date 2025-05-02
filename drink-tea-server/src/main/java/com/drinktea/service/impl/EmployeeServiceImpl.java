0fc0f8039ef355e6a13e9305dbc8d47d709cf94c
Qianvenh <3294416164@qq.com>
2025-05-02 21:30:43 +0800

    修改gitignore

--- drink-tea-server/src/main/java/com/drinktea/service/impl/EmployeeServiceImpl.java
+++ drink-tea-server/src/main/java/com/drinktea/service/impl/EmployeeServiceImpl.java
@@ -73,12 +73,11 @@ public class EmployeeServiceImpl implements EmployeeService {
                 DigestUtils.md5DigestAsHex(
                         PasswordConstant.DEFAULT_PASSWORD.getBytes()));
         // 设置创建时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setCreateTime(LocalDateTime.now());
+//        employee.setUpdateTime(LocalDateTime.now());
         // 设置当前记录的创建者和更新者的id
-        // TODO 后期要改为当前登录用户的id
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+//        employee.setCreateUser(BaseContext.getCurrentId());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
         // 在持久层中进行操作
         employeeMapper.insert(employee);
     }
@@ -132,8 +131,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     public void update(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+//        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.update(employee);
     }
 }
