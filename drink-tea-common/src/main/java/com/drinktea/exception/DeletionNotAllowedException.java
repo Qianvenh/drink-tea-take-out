9b2af5051c9cb02be52d7467ee59e98582a66d88
Qianvenh <3294416164@qq.com>
2025-05-02 15:53:58 +0800

    分类管理模块功能代码

---
+++ drink-tea-common/src/main/java/com/drinktea/exception/DeletionNotAllowedException.java
@@ -0,0 +1,9 @@
+package com.drinktea.exception;
+
+public class DeletionNotAllowedException extends BaseException {
+
+    public DeletionNotAllowedException(String msg) {
+        super(msg);
+    }
+
+}
